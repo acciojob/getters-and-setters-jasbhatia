7497efcf3d31bd1110c253ad88da07a43c5d83cf
JASPREET KAUR <94101161+jasbhatia@users.noreply.github.com>
2023-05-11 19:29:45 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,30 +1,33 @@
 //complete this code
 class Person {
 constructor(name, age) {
-this.name = name;
-this.age = age;
-}
+    this._name = name;
+    this._age = age;
+  }
 
-get namee() {
-return this.name;
-}
+  get name() {
+    return this._name;
+  }
 
-set agee(age) {
-this.age = age;
-}
-}
+  get age() {
+    return this._age;
+  }
 
+  set age(value) {
+    this._age = value;
+  }
+}
 class Student extends Person {
 study() {
-console.log(`${this.name} is studying.`);
+    console.log(`${this.name} is studying`);
+  }
 }
+class Teacher extends Person {
+ teach() {
+    console.log(`${this.name} is teaching`);
+  }
 }
 
-class Teacher extends Person {
-teach() {
-console.log("${this.name} is teaching.");
-}
-}
 // Do not change the code below this line
 window.Person = Person;
 window.Student = Student;
